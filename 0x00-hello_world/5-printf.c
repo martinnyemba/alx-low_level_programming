f256ca8aef6045ce9d2e619d865ccaf9e1921b27
Eng Nyemba Martin <martinnyemba@gmail.com>
2022-09-08 15:44:51 +0000

    added sript for task 4 and 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints a string
+ *
+ * return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
