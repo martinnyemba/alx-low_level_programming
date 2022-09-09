7b67b487a255b98938d2d2806ee9b44225504fb3
Eng Nyemba Martin <martinnyemba@gmail.com>
2022-09-09 20:38:36 +0000

    added all the tasks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-*Description: This program will assign a random number
-* to the variable n each time it is executed 
-* Return: Always 0 (Success)
-*/
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
 int main(void)
 {
 	int n;
