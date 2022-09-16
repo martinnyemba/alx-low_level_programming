ad89d9738a6e2202c6c4e6b87c45e9e3c071070a
Eng Nyemba Martin <martinnyemba@gmail.com>
2022-09-16 11:41:51 +0000

    fixed task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-	positive_or_negative();
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
