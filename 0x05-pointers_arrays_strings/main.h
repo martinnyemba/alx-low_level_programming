18978f1c27f3d7993b7e73f49a443f929c06aa63
Eng Nyemba Martin <martinnyemba@gmail.com>
2022-09-19 18:00:36 +0000

    Added all the mandatory tasks

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* prototype for the puchar function */
+int _putchar(char c);
+
+/* takes a pointer to an int */
+void reset_to_98(int *n);
+
+/* swaps the values of two integers */
+void swap_int(int *a, int *b);
+
+/* returns the length of a string */
+int _strlen(char *s);
+
+/* prints a string, followed by a new line, to stdout */
+void _puts(char *str);
+
+/* prints a string, in reverse, followed by a new line */
+void print_rev(char *s);
+
+/* prints every other character of a string */
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+
+#endif
